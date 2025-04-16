84f5b9c101549da35d79bd7bb5f210e9317b800d
unknown <you@example.com>
2025-04-16 16:13:16 +0900

    test

---
+++ src/test1/test1.java
@@ -0,0 +1,15 @@
+package test1;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+public class test1 {
+
+	public static void main(String[] args) throws IOException {
+		int[] list = { 1, 1, 2, 3, 5 };
+		System.out.println("123");
+	}
+
+}
